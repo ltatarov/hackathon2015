98a735f693675d3aa9ee25645705d7f029b62d5e
Egemen Kalyoncu <ekalyoncu@marktplaats.nl>
2015-05-07 12:57:20 +0200

    added check befire asking user name

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
-	// TODO only do this if user is not "logged in"
-	$('#login').modal('show') ;
 
-	$('#submitName').click(function(){
-		var name = $("#nameTextbox").val();
-		if (name ) {
-			$.post( "http://localhost:5000/register", {'name': name}, function( data ) {
-				$('#login').modal('hide') ;				
-			});        
-		} else {
-			alert("Please enter a username");
-		}      
-	});	
-})
+    if (document.cookie.indexOf("mp-hackathon-user") < 0) {
+        $('#login').modal('show') ;
+
+		$('#submitName').click(function(){
+			var name = $("#nameTextbox").val();
+			if (name ) {
+				$.post( "http://localhost:5000/register", {'name': name}, function( data ) {
+					$('#login').modal('hide') ;				
+				});        
+			} else {
+				alert("Please enter a username");
+			}      
+		});	
+	}
+	
+});

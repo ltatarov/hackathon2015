89f66ae40c966ef40275cbd909335963ad50c380
Pedro Taborda <ptaborda@ebay.com>
2015-05-07 14:24:02 +0200

    added back ids (conflicts suck)

--- js/index.js
+++ js/index.js
@@ -1,63 +1,63 @@
 $(document).ready(function() {
 
-	// registration flow
-    if (document.cookie.indexOf("mp-hackathon-user") < 0) {
+  // registration flow
+  // if (document.cookie.indexOf("mp-hackathon-user") < 0) {
+  //   $('#mainContainer').hide();
+  //   $('#login').modal('show') ;
+  //
+  //   $('#submitName').click(function(){
+  //     var name = $("#nameTextbox").val();
+  //     if (name ) {
+  //       $.post( "/api/register", {'name': name}, function( data ) {
+  //         $('#login').modal('hide');
+  //       });
+  //       $('#mainContainer').show();
+  //       getNextItem();
+  //     } else {
+  //       alert("Please enter a username");
+  //     }
+  //   });
+  // } else {
+  //   getNextItem();
+  // }
 
-    	$('#mainContainer').hide();
+  getNextItem();
 
-		$('#login').modal('show') ;
+  function getNextItem() {
+    $.ajax({
+      type: 'GET',
+      url: '/api/item',
+      success: function(data){
+        $("#image").attr("src", data.photo);
+        $("#title").text(data.title);
+        $("#description").text(data.description);
+        $("#itemId").val(data.id);
+        $("#answerText").val("");
+      },
+      statusCode: {
+        404: function() {
+          // handle 404
+          $('#mainContainer').hide();
+          alert("No more items available, thanks for playing :)");
+        }
+      }
+    });
 
-		$('#submitName').click(function(){
-			var name = $("#nameTextbox").val();
-			if (name ) {
-				$.post( "/api/register", {'name': name}, function( data ) {
-					$('#login').modal('hide');				
-				}); 
-				$('#mainContainer').show();   
-				getNextItem();
-			} else {
-				alert("Please enter a username");
-			}  
-		});	
-	} else {
-		getNextItem();
-	}
+  }
 
-	function getNextItem() {
-		$.ajax({
-		    type: 'GET',
-		    url: '/api/item',
-		    success: function(data){
-		        $("#image").attr("src", data.photo);
-		        $("#title").text(data.title);
-		        $("#description").text(data.description);
-		        $("#itemId").val(data.id);
-				$("#answerText").val("");
-		    },
-		    statusCode: {
-	    		404: function() {
-		    		// handle 404
-		    		$('#mainContainer').hide();
-		    		alert("No more items available, thanks for playing :)");
-		    	}
-		    }
-		});
+  $("#submitAnswer").click(function() {
+    var answer = $("#answerText").val();
+    console.log(answer);
+    if (answer && !isNaN(answer)) {
+      $.post("/api/answer", {"id": $("#itemId").val(), "price": answer});
+      getNextItem();
+    } else {
+      alert("please enter a guess (or skip)");
+    }
+  });
 
-	}
+  $("#skipItem").click(function() {
+    getNextItem();
+  });
 
-	$("#submitAnswer").click(function() {
-		var answer = $("#answerText").val();
-		console.log(answer);
-		if (answer && !isNaN(answer)) {
-			$.post("/api/answer", {"id": $("#itemId").val(), "price": answer});
-			getNextItem();
-		} else {
-			alert("please enter a guess (or skip)");
-		}		
-	});
-
-	$("#skipItem").click(function() {
-		getNextItem();
-	});
-	
 });

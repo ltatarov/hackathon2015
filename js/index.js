c9c8e17156f4fdfc17c32d63a9bfee88f3d9cb3c
ltatarov <ltatarov@ebay.com>
2015-05-07 12:25:35 +0200

    JS for username

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-  // TODO only do this if user is not "logged in"
-  // $('#login').modal('show') ;
+	// TODO only do this if user is not "logged in"
+	$('#login').modal('show') ;
+
+	$('#submitName').click(function(){
+		var name = $("#nameTextbox").val();
+		if (name ) {
+			$.post( "http://localhost:5000/register", {'name': name}, function( data ) {
+				$('#login').modal('hide') ;				
+			});        
+		} else {
+			alert("Please enter a username");
+		}      
+	});	
 })
